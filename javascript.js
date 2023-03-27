fe086cadd5f10e46c78943cc91c026303b34127f
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-03-28 00:58:55 +0500

    Add preserve data in local storage feature

--- javascript.js
+++ javascript.js
@@ -3,49 +3,47 @@ let array = [];
 function addBook() {
   let title = document.getElementById("title_input");
   let author = document.getElementById("author_input");
+  if (title.value !== "" && author.value !== "") {
+    let book_list = document.getElementById("book_list");
 
-  let book_list = document.getElementById("book_list");
+    let book_content = document.createElement("section");
 
-  let book_content = document.createElement("section");
-  book_content.style.border = "thin solid grey";
-  book_content.style.margin = "30px";
+    let book_title = document.createElement("h3");
+    let book_author = document.createElement("h3");
+    let remove_button = document.createElement("button");
+    let hr = document.createElement("hr");
+    book_content.style.width = "280px";
 
-  let book_title = document.createElement("h3");
-  let book_author = document.createElement("h3");
-  let remove_button = document.createElement("button");
-  let hr = document.createElement("hr");
+    book_list.appendChild(book_content);
+    book_content.appendChild(book_title);
+    book_content.appendChild(book_author);
+    book_content.appendChild(remove_button);
+    book_content.appendChild(hr);
 
-  book_list.appendChild(book_content);
-  book_content.appendChild(book_title);
-  book_content.appendChild(book_author);
-  book_content.appendChild(remove_button);
-  book_content.appendChild(hr);
+    remove_button.innerText = "Remove";
 
-  remove_button.innerText = "Remove";
+    book_title.innerText = "Book title: " + title.value;
+    book_author.innerText = "Book author: " + author.value;
 
-  book_title.innerText = "Book title: " + title.value;
-  book_author.innerText = "Book author: " + author.value;
+    let i = array.length;
+    book_content.id = array.length;
 
-  let i = array.length;
-  book_content.id = array.length;
+    let obj = { id: i, title: title.value, author: author.value };
+    array.push(obj);
 
-  let obj = { id: i, title: title.value, author: author.value };
-  array.push(obj);
-  console.log(array);
-  console.log("array length: " + array.length);
+    remove_button.onclick = function removeBook() {
+      let id = book_content.id;
 
-  remove_button.onclick = function removeBook() {
-    let id = book_content.id;
+      const indexOfObject = array.findIndex((object) => {
+        book_content.remove();
+        return object.id === id;
+      });
 
-    const indexOfObject = array.findIndex((object) => {
-      book_content.remove();
-      return object.id === id;
-    });
+      array.splice(indexOfObject, 1);
+    };
 
-    console.log(indexOfObject);
+    //   Preserve data in the browser
 
-    array.splice(indexOfObject, 1);
-
-    console.log(array);
-  };
+    localStorage.setItem("bookDetails", JSON.stringify(array));
+  }
 }
