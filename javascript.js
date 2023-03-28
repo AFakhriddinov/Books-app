16117a8916ae7b0a2fd48a62a6cceeef8b50e421
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-03-28 21:09:59 +0500

    Fix eslint errors

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-const arrays = JSON.parse(localStorage.getItem("bookDetails")) || [];
+const arrays = JSON.parse(localStorage.getItem('bookDetails')) || [];
 
-const title = document.getElementById("title_input");
-const author = document.getElementById("author_input");
-const addButton = document.getElementById("add_button");
+const title = document.getElementById('title_input');
+const author = document.getElementById('author_input');
+const addButton = document.getElementById('add_button');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener('click', () => {
   if (title.value && author.value) {
     let i;
     if (arrays.length === null) {
@@ -14,21 +14,20 @@ addButton.addEventListener("click", () => {
     }
 
     arrays.push({ id: i, title: title.value, author: author.value });
-    location.reload();
-    localStorage.setItem("bookDetails", JSON.stringify(arrays));
-    location.reload();
+    localStorage.setItem('bookDetails', JSON.stringify(arrays));
+    window.location.reload();
   }
 });
 
 function removeBook(id) {
   const remainedBooks = arrays.filter((array) => array.id !== id);
-  localStorage.setItem("bookDetails", JSON.stringify(remainedBooks));
-  location.reload();
+  localStorage.setItem('bookDetails', JSON.stringify(remainedBooks));
+  window.location.reload();
 }
 
 function displayBooks() {
-  const bookContent = document.getElementById("book_list");
-  for (let i = 0; i < arrays.length; i++) {
+  const bookContent = document.getElementById('book_list');
+  for (let i = 0; i < arrays.length; i += 1) {
     bookContent.innerHTML += `<div><p>
         Book title: ${arrays[i].title} <br />
          Book author: ${arrays[i].author} 
@@ -42,10 +41,10 @@ function displayBooks() {
   }
 }
 
-const bookListSection = document.createElement("section");
-bookListSection.id = "list";
-bookListSection.innerHTML = "<h1>Awesome books</h1>";
+const bookListSection = document.createElement('section');
+bookListSection.id = 'list';
+bookListSection.innerHTML = '<h1>Awesome books</h1>';
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   displayBooks();
 });
