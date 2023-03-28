33eaa7229cbdb2deb8ac81a485972dc0d6ad0950
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-03-28 16:36:26 +0500

    Fix local storage data persistency

--- javascript.js
+++ javascript.js
@@ -1,56 +1,52 @@
-const array = [];
+const arrays = JSON.parse(localStorage.getItem("bookDetails")) || [];
 
-function addBook() {
-  const title = document.getElementById('title_input');
-  const author = document.getElementById('author_input');
-  if (title.value !== '' && author.value !== '') {
-    const bookList = document.getElementById('book_list');
+const title = document.getElementById("title_input");
+const author = document.getElementById("author_input");
+const addButton = document.getElementById("add_button");
 
-    const bookContent = document.createElement('section');
+addButton.addEventListener("click", () => {
+  if (title.value && author.value) {
+    let i;
+    if (arrays.length === null) {
+      i = 0;
+    } else {
+      i = arrays.length;
+    }
 
-    const bookTitle = document.createElement('h3');
-    const bookAuthor = document.createElement('h3');
-    const removeButton = document.createElement('button');
-    const hr = document.createElement('hr');
-    bookContent.style.width = '280px';
-    removeButton.style.margin = '20px';
+    arrays.push({ id: i, title: title.value, author: author.value });
+    location.reload();
+    localStorage.setItem("bookDetails", JSON.stringify(arrays));
+    location.reload();
+  }
+});
 
-    bookList.appendChild(bookContent);
-    bookContent.appendChild(bookTitle);
-    bookContent.appendChild(bookAuthor);
-    bookContent.appendChild(removeButton);
-    bookContent.appendChild(hr);
+console.log(arrays.length);
+function removeBook(id) {
+  const remainedBooks = arrays.filter((array) => array.id !== id);
+  localStorage.setItem("bookDetails", JSON.stringify(remainedBooks));
+  location.reload();
+}
 
-    removeButton.innerText = 'Remove';
-
-    bookTitle.innerText = `Book title: ${title.value}`;
-    bookAuthor.innerText = `Book author: ${author.value}`;
-
-    const i = array.length;
-    bookContent.id = array.length;
-
-    const obj = { id: i, title: title.value, author: author.value };
-    array.push(obj);
-
-    removeButton.onclick = function removeBook() {
-      const { id } = bookContent;
-
-      const indexOfObject = array.findIndex((object) => {
-        bookContent.remove();
-        return object.id === id;
-      });
-
-      array.splice(indexOfObject, 1);
-    };
-
-    //   Preserve data in the browser
-
-    localStorage.setItem('bookDetails', JSON.stringify(array));
+function displayBooks() {
+  const bookContent = document.getElementById("book_list");
+  for (let i = 0; i < arrays.length; i++) {
+    bookContent.innerHTML += `<div><p>
+        Book title: ${arrays[i].title} <br />
+         Book author: ${arrays[i].author} 
+      </p>
+      <br/>
+      <button onclick="removeBook(${arrays[i].id})">Remove</button>
+      <br/><br/>
+      <hr />
+      <br/>
+    </div>`;
   }
 }
 
-const bookListSection = document.createElement('section');
-bookListSection.id = 'list';
-bookListSection.innerHTML = '<h1>Awesome books</h1>';
+const bookListSection = document.createElement("section");
+bookListSection.id = "list";
+bookListSection.innerHTML = "<h1>Awesome books</h1>";
 
-addBook();
+window.addEventListener("load", () => {
+  displayBooks();
+});
