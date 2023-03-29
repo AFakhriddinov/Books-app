078aacc1f3108d38421610733b88517053e90b74
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-03-29 13:48:43 +0500

    Fix unique ID for books created

--- javascript.js
+++ javascript.js
@@ -1,41 +1,34 @@
-// const arrays = JSON.parse(localStorage.getItem("bookDetails")) || [];
-
-const title = document.getElementById('title_input');
-const author = document.getElementById('author_input');
-const addButton = document.getElementById('add_button');
+const title = document.getElementById("title_input");
+const author = document.getElementById("author_input");
+const addButton = document.getElementById("add_button");
 
 class BookLibrary {
   constructor() {
-    this.arrays = JSON.parse(localStorage.getItem('bookDetails')) || [];
+    this.arrays = JSON.parse(localStorage.getItem("bookDetails")) || [];
   }
 
   addBooks(title, author, id) {
     const addedNew = { id, title, author };
     this.arrays.push(addedNew);
-    localStorage.setItem('bookDetails', JSON.stringify(this.arrays));
+    localStorage.setItem("bookDetails", JSON.stringify(this.arrays));
   }
 
   eraseBook(id) {
     const { arrays } = this;
     const erased = arrays.filter((array) => array.id !== id);
-    localStorage.setItem('bookDetails', JSON.stringify(erased));
+    localStorage.setItem("bookDetails", JSON.stringify(erased));
   }
 
-  persentBooks() {
+  presentBooks() {
     return this.arrays;
   }
 }
 
 const collection = new BookLibrary();
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener("click", () => {
   if (title.value && author.value) {
-    let i;
-    if (collection.arrays.length === null) {
-      i = 0;
-    } else {
-      i = collection.arrays.length;
-    }
+    let i = Date.now();
     collection.addBooks(title.value, author.value, i);
     window.location.reload();
   }
@@ -48,8 +41,8 @@ function removeBook(id) {
 }
 
 function displayBooks() {
-  const bookContent = document.getElementById('book_list');
-  const allCollection = collection.persentBooks();
+  const bookContent = document.getElementById("book_list");
+  const allCollection = collection.presentBooks();
   for (let i = 0; i < allCollection.length; i += 1) {
     if (i % 2 === 0) {
       bookContent.innerHTML += `<div class="bookContainer" id="odd"><p class="bookInfo">
@@ -71,10 +64,10 @@ function displayBooks() {
   }
 }
 
-const bookListSection = document.createElement('section');
-bookListSection.id = 'list';
-bookListSection.innerHTML = '<h1>Awesome books</h1>';
+const bookListSection = document.createElement("section");
+bookListSection.id = "list";
+bookListSection.innerHTML = "<h1>Awesome books</h1>";
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   displayBooks();
 });
